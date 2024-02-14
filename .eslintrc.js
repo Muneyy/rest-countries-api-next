15a9c7db89172482785076baf1598d1de2a993dd
Muneyy <muneyyyy@gmail.com>
2024-02-14 18:27:18 +0800

    Setup pre-commit hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,34 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  "extends": [
+  extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "plugin:jsx-a11y/recommended",
   ],
-  "overrides": [
+  overrides: [
     {
-      "env": {
-        "node": true
+      env: {
+        node: true,
       },
-      "files": [
-        ".eslintrc.{js,cjs}"
-      ],
-      "parserOptions": {
-        "sourceType": "script"
-      }
-    }
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  "plugins": [
-    "@typescript-eslint",
-    "react",
-    "jsx-a11y"
-  ],
-  "rules": {
-    "indent": ["error", 2],
+  plugins: ["@typescript-eslint", "react", "jsx-a11y"],
+  rules: {
+    indent: ["error", 2],
     "react/react-in-jsx-scope": "off",
     "react/button-has-type": "error",
-    "jsx-a11y/aria-role": [
-      "error",
-      {
-        "ignoreNonDOM": true
-      }
-    ]
-  }
-}
+  },
+};
