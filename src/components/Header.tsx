3f4e401941417fb3205fd032189b267aa0cd654f
Muneyy <muneyyyy@gmail.com>
2024-02-14 19:18:22 +0800

    feat: create layout for header
    note: changing color theme not yet implemented

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,15 @@
-'use client';
-
 import styles from './Header.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon } from '@fortawesome/free-solid-svg-icons';
-import Link from 'next/link';
 
 export default function Header() {
-  function toggleThemeClick() {
-    const currentTheme = document.documentElement.getAttribute('data-theme');
-    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-    document.documentElement.setAttribute('data-theme', newTheme);
-  }
-
   return (
     <section className={styles.headerContainer}>
-      <Link
-        className={styles.headerTitle}
-        href={'/'}
-        aria-label="Link to homepage"
-      >
-        Where in the world?
-      </Link>
+      <span className={styles.headerTitle}>Where in the world?</span>
       <button
         type="button"
         aria-label="Toggle dark mode"
         className={styles.themeButton}
-        onClick={toggleThemeClick}
       >
         <FontAwesomeIcon icon={faMoon} />
         <p>Dark Mode</p>
