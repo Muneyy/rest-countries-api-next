a465400c579c53d1b0781f93fa933e442849461d
Muneyy <muneyyyy@gmail.com>
2024-02-15 22:27:01 +0800

    feat: sort currencies, languages, and borders, also add commas to population

--- src/app/[countryCode]/page.tsx
+++ src/app/[countryCode]/page.tsx
@@ -3,6 +3,8 @@ import styles from './page.module.scss';
 import Link from 'next/link';
 import BackButton from '@/components/BackButton';
 import { notFound } from 'next/navigation';
+import renderKeyValuePairs from '@/helperFunctions/renderKeyValuePairs';
+import commafy from '@/helperFunctions/commafy';
 
 async function getData(countryCode: string) {
   try {
@@ -33,73 +35,51 @@ export default async function CountryDetails({
     return notFound();
   }
 
-  const fetchedCountry = await data;
+  // const fetchedCountry = await data;
+  const { flags, name, altSpellings, population, region, subregion, capital, tld, currencies, languages, borders } =
+    await data;
 
-  if (fetchedCountry && fetchedCountry.name) {
-    return (
-      <section className={styles.container}>
-        <BackButton />
-        <Image
-          src={fetchedCountry.flags?.svg}
-          alt={fetchedCountry.flags.alt}
-          width={320}
-          height={200}
-          className={styles.flagImage}
-        />
-        <h1>{fetchedCountry.name.common}</h1>
+  return (
+    <section className={styles.container}>
+      <BackButton />
+      <Image src={flags.svg} alt={flags.alt} width={320} height={200} className={styles.flagImage} />
+      <h1>{name.common}</h1>
+      <p>
+        <b>Native Name:</b> {altSpellings[1]}
+      </p>
+      <p>
+        <b>Population:</b> {commafy(population)}
+      </p>
+      <p>
+        <b>Region:</b> {region}
+      </p>
+      <p>
+        <b>Sub Region:</b> {subregion}
+      </p>
+      <p>
+        <b>Capital:</b> {capital[0]}
+      </p>
+      <br />
+      <p>
+        <b>Top Level Domain:</b> {tld[0]}
+      </p>
+      {renderKeyValuePairs(currencies, 'Currencies')}
+      {renderKeyValuePairs(languages, 'Languages')}
+      <div>
         <p>
-          <b>Native Name:</b> {fetchedCountry.altSpellings[1]}
-        </p>
-        <p>
-          <b>Population:</b> {fetchedCountry.population}
-        </p>
-        <p>
-          <b>Region:</b> {fetchedCountry.region}
-        </p>
-        <p>
-          <b>Sub Region:</b> {fetchedCountry.subregion}
-        </p>
-        <p>
-          <b>Capital:</b> {fetchedCountry.capital[0]}
+          <b>Border Countries: </b>
         </p>
         <br />
-        <p>
-          <b>Top Level Domain:</b> {fetchedCountry.tld[0]}
-        </p>
-        <p>
-          <b>Currencies: </b>
-          {Object.entries(fetchedCountry.currencies).map(([key, value], index, array) => (
-            <span key={key}>
-              {(value as { name: string }).name as string}
-              {index === array.length - 1 ? null : ', '}
-            </span>
-          ))}
-        </p>
-        <p>
-          <b>Languages: </b>
-          {Object.entries(fetchedCountry.languages).map(([key, value], index, array) => (
-            <span key={key}>
-              {value as string}
-              {index === array.length - 1 ? null : ', '}
-            </span>
-          ))}
-        </p>
-        <div>
-          <p>
-            <b>Border Countries: </b>
-          </p>
-          <br />
-          <div className={styles.bordersContainer}>
-            {fetchedCountry.borders.map((border: string) => {
-              return (
-                <Link href={`/${border}`} key={border} aria-label={`Link to ${border}`} className={styles.borderLink}>
-                  <button type="button">{border}</button>
-                </Link>
-              );
-            })}
-          </div>
+        <div className={styles.bordersContainer}>
+          {borders.sort().map((border: string) => {
+            return (
+              <Link href={`/${border}`} key={border} aria-label={`Link to ${border}`} className={styles.borderLink}>
+                <button type="button">{border}</button>
+              </Link>
+            );
+          })}
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
 }
---
+++ src/helperFunctions/renderKeyValuePairs.tsx
@@ -0,0 +1,26 @@
+export default function renderKeyValuePairs(data: Record<string, { name: string } | string>, label: string) {
+  const textInputs: string[] = [];
+  const tempArr = Object.entries(data);
+
+  // extract text values from the objects
+  tempArr.forEach((item) => {
+    const value = label === 'Currencies' ? (item[1] as { name: string })?.name : (item[1] as string);
+    if (value) {
+      textInputs.push(value);
+    }
+  });
+
+  const sortedInputs = textInputs.sort();
+
+  return (
+    <p>
+      <b>{label}: </b>
+      {sortedInputs.map((key, index, array) => (
+        <span key={key}>
+          {key}
+          {index === array.length - 1 ? null : ', '}
+        </span>
+      ))}
+    </p>
+  );
+}
